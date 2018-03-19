f1ee312f031c7ac7605f497c9814ef0be40ecfb1
Sam De Legge <samd@Sams-MacBook-Pro.local>
2018-03-18 20:15:51 -0700

    second push

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+
+//I need to set up the randomly generated number between 19 - 120
+
+//at the start of the game
+
+
+
+
+
+
+
+//I need to add a random value of 1-12 to each of the 4 crystals
+
+
+//when user clicks on crystal it will add specific number of points
+//to score
+
+
+//game will hide this amount of points until crystal is clicked
+
+
+//player wins if their total score matchs random number
+
+//player loses if score goes above random number
+
+
+//game restarts upon win/loss
+
+//when a new game begins the player should get a new random number
+
+
+//all 4 crystals will have new values each game
+
+
+//score and score counter will reset to 0
+
+
+//app should show number of games the player wins and loses
