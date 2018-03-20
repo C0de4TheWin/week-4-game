e9dda4007a4cf72f08a15e1719829660d9d06aba
Sam De Legge <samd@Sams-MacBook-Pro.local>
2018-03-19 21:19:22 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,108 +4,111 @@
 //at the start of the game
 
 var starting = Math.floor((Math.random() * 120) + 19);
-var userTotal= 0; 
-var wins= 0;
+var userTotal = 0;
+var wins = 0;
 var losses = 0;
 
-
-console.log(starting)
+console.log(starting);
 
 
 
 $('#randomNumber').text(starting);
 
 
-
-
-
-
 //I need to add a random value of 1-12 to each of the 4 crystals
-var num1= Math.floor(Math.random()*11+1)
-var num2= Math.floor(Math.random()*11+1)
-var num3= Math.floor(Math.random()*11+1)
-var num4= Math.floor(Math.random()*11+1)
+var num1 = Math.floor(Math.random() * 11 + 1);
+var num2 = Math.floor(Math.random() * 11 + 1);
+var num3 = Math.floor(Math.random() * 11 + 1);
+var num4 = Math.floor(Math.random() * 11 + 1);
 
 
 /////////////variables for scores
 
-$('#playerWins').text(wins);
-$('#playerLosses').text(losses);
-
-//resets the game
-function reset(){
-    Random=Math.floor(Math.random()*101+19);
-    console.log(Random)
-    $('#randomNumber').text(Random);
-    num1= Math.floor(Math.random()*11+1);
-    num2= Math.floor(Math.random()*11+1);
-    num3= Math.floor(Math.random()*11+1);
-    num4= Math.floor(Math.random()*11+1);
-    userTotal= 0;
-    $('#finalTotal').text(userTotal);
-    } 
-//adds the wins to the userTotal
-function yay(){
-alert("You won!");
-wins++; 
 $('#numberWins').text(wins);
-reset();
-}
-//addes the losses to the userTotal
-function loser(){
-alert ("You lose!");
-losses++;
 $('#numberLosses').text(losses);
-reset()
+
+//reset
+function reset() {
+    Random = Math.floor(Math.random() * 101 + 19);
+    console.log(Random);
+    $('#randomNumber').text(Random);
+    num1 = Math.floor(Math.random() * 11 + 1);
+    num2 = Math.floor(Math.random() * 11 + 1);
+    num3 = Math.floor(Math.random() * 11 + 1);
+    num4 = Math.floor(Math.random() * 11 + 1);
+    userTotal = 0;
+    $('#finalTotal').text(userTotal);
 }
-//sets up click for jewels
-$('#one').on ('click', function(){
-  userTotal = userTotal + num1;
-  console.log("New userTotal= " + userTotal);
-  $('#finalTotal').text(userTotal); 
-        //sets win/lose conditions
-      if (userTotal == Random){
+
+////adds wins to total
+function yay() {
+    alert("You won!");
+    wins++;
+    $('#numberWins').text(wins);
+    reset();
+}
+
+
+////adds losses to total
+function loser() {
+    alert("You lose!");
+    losses++;
+    $('#numberLosses').text(losses);
+    reset()
+}
+
+
+////the click for jewels
+$('#one').on('click', function () {
+    userTotal = userTotal + num1;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal);
+
+
+
+    ////sets win or loss scenario
+    if (userTotal == Random) {
         yay();
-      }
-      else if ( userTotal > Random){
+    }
+    else if (userTotal > Random) {
         loser();
-      }   
-})  
-$('#two').on ('click', function(){
-  userTotal = userTotal + num2;
-  console.log("New userTotal= " + userTotal);
-  $('#finalTotal').text(userTotal); 
-      if (userTotal == Random){
+    }
+})
+$('#two').on('click', function () {
+    userTotal = userTotal + num2;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal);
+    if (userTotal == Random) {
         yay();
-      }
-      else if ( userTotal > Random){
+    }
+    else if (userTotal > Random) {
         loser();
-      } 
-})  
-$('#three').on ('click', function(){
-  userTotal = userTotal + num3;
-  console.log("New userTotal= " + userTotal);
-  $('#finalTotal').text(userTotal);
-//sets win/lose conditions
-        if (userTotal == Random){
+    }
+})
+$('#three').on('click', function () {
+    userTotal = userTotal + num3;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal);
+    //sets win/lose conditions
+    if (userTotal == Random) {
         yay();
-      }
-      else if ( userTotal > Random){
+    }
+    else if (userTotal > Random) {
         loser();
-      } 
-})  
-$('#four').on ('click', function(){
-  userTotal = userTotal + num4;
-  console.log("New userTotal= " + userTotal);
-  $('#finalTotal').text(userTotal); 
-    
-        if (userTotal == Random){
+    }
+})
+$('#four').on('click', function () {
+    userTotal = userTotal + num4;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal);
+
+    if (userTotal == Random) {
         yay();
-      }
-      else if ( userTotal > Random){
+    }
+    else if (userTotal > Random) {
         loser();
-      }
-});    
+    }
+});
 
 
 //when user clicks on crystal it will add specific number of points
